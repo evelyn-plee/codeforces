aeeea9223cff63857605c8111c93a0b74b9179f0
evelyn-plee <evelyn.plee@gmail.com>
2019-02-20 20:06:22 +1300

    Create main.cpp

---
+++ 388A_BachGoldProblem/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 0;
+    cin >> n;
+    cout << n/2 << endl;
+    while( n > 3){
+        cout << 2 << " ";
+        n -= 2;
+    }
+    cout << n;
+    return 0;
+}
