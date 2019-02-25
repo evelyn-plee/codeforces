82e9b9be3ea49cd64cd91514dde963708e7d328e
evelyn-plee <evelyn.plee@gmail.com>
2019-02-25 18:16:52 +1300

    Create main.cpp

---
+++ 550A_TwoSubstrings/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+
+    if(s.find("AB")!=-1 && s.find("BA",s.find("AB")+2)!=-1 || s.find("BA")!=-1 && s.find("AB",s.find("BA")+2)!=-1) cout<<"YES";
+    else cout << "NO"; 
+    return 0;
+}
